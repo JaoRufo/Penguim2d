46952723ab03c409fd6af04b55f5d6bd7c0035c3
Joao Rufo <joaovictorrufopereira44@gmail.com>
2026-05-19 12:38:20 -0300

    Feat - Novas animações Player e inserção inicial do Enemy (Skeleton)

---
+++ scripts/skeleton.gd
@@ -0,0 +1,84 @@
+extends CharacterBody2D
+
+enum SkeletonState {
+	walk,
+	hurt
+}
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hitbox: Area2D = $Hitbox
+@onready var wall_detector: RayCast2D = $WallDetector
+@onready var ground_detector: RayCast2D = $GroundDetector
+
+const SPEED = 10.0
+const JUMP_VELOCITY = -400.0
+
+var status: SkeletonState
+
+var direction = 1
+
+func _ready() -> void:
+	go_to_walk_state()
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	match status:
+		SkeletonState.walk:
+			walk_state(delta)
+		SkeletonState.hurt:
+			hurt_state(delta)
+
+	move_and_slide()
+	
+func go_to_walk_state():
+	status = SkeletonState.walk
+	anim.play("walk")
+	
+func go_to_hurt_state():
+	status = SkeletonState.hurt
+	anim.play("hurt")
+	hitbox.process_mode = Node.PROCESS_MODE_DISABLED
+	velocity = Vector2.ZERO
+	
+func walk_state(_delta):
+	velocity.x = SPEED * direction
+	
+	if wall_detector.is_colliding():
+		scale.x *= -1
+		direction *= -1
+		
+	if !ground_detector.is_colliding():
+		scale.x *= -1
+		direction *= -1
+		
+	
+func hurt_state(_delta):
+	pass
+	
+func take_damage():
+	go_to_hurt_state()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
